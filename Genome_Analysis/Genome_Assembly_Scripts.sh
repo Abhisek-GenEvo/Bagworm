5ba67e763264a8195b16e5e12420dbf8008179b0
Abhisek-GenEvo <chakrabortyabhi2013@gmail.com>
2025-06-30 12:09:37 +0200

    Update Genome_Assembly_Scripts.sh

--- Genome_Analysis/Genome_Assembly_Scripts.sh
+++ Genome_Analysis/Genome_Assembly_Scripts.sh
@@ -1,19 +1,19 @@
 #10X Genomics data assembly
-supernova run --id=Assembly_Output --sample=BW,bw,Bw --fastqs=input1,input2,input3 --maxreads=all --localcores=40 --localmem=980 --nodebugmem --accept-extreme-coverage
-supernova mkoutput --style=pseudohap2 --asmdir=Assembly_Output/outs/assembly --outprefix=pseudo2_output --minsize=0 --index
+supernova run --id=EC_assembly_output --sample=EC1,ec2,Ec3 --fastqs=EC_input1,EC_input2,EC_input3 --maxreads=all --localcores=40 --localmem=980 --nodebugmem --accept-extreme-coverage
+supernova mkoutput --style=pseudohap2 --asmdir=EC_assembly_output/outs/assembly --outprefix=pseudo2_output_EC --minsize=0 --index
 
-longranger basic --id=longranger_out_BW --fastqs=/DATA/BW_abhisek/BW_input1,/DATA/BW_abhisek/BW_input2,/DATA/BW_abhisek/BW_input3 --sample=BW,bw,Bw --localcores=36 --localmem=500
-samtools faidx pseudo2_BW_All_output_single1.fasta
-bwa index pseudo2_BW_All_output_single1.fasta
-bwa mem -t46 -p -C pseudo2_BW_All_output_single1.fasta /DATA/BW_abhisek/longranger_out_BW/outs/barcoded.fastq.gz | samtools sort -@8 -tBX -o draft.reads.sortbx.bam
+longranger basic --id=longranger_out_EC --fastqs=/DATA/EC/EC_input1,/DATA/EC/EC_input2,/DATA/EC/EC_input3 --sample=EC1,ec2,Ec3 --localcores=36 --localmem=500
+samtools faidx pseudo2_output_EC.fasta
+bwa index pseudo2_output_EC.fasta
+bwa mem -t46 -p -C pseudo2_output_EC.fasta /DATA/EC/longranger_out_EC/outs/barcoded.fastq.gz | samtools sort -@8 -tBX -o draft.reads.sortbx.bam
 tigmint-molecule draft.reads.sortbx.bam | sort -k1,1 -k2,2n -k3,3n > draft.reads.molecule.bed
-tigmint-cut -p46 -o draft.tigmint.fa pseudo2_BW_All_output_single1.fasta draft.reads.molecule.bed
+tigmint-cut -p46 -o draft.tigmint.fa pseudo2_output_EC.fasta draft.reads.molecule.bed
 
 #Illumina assembly
-spades.py --only-assembler -1 BW_illumina_10x_R1.fastq -2 BW_illumina_10x_R2.fastq -s BW_illu_10x_unpaired.fastq -k 107 -t 68 -m 970 -o SPADES_Output_final_BW_107
+spades.py --only-assembler -1 EC_illumina_R1.fastq -2 EC_illumina_R2.fastq -s EC_illumina_unpaired.fastq -k 107 -t 68 -m 970 -o SPADES_output_EC_k107
 
 #Scaffolding of both assemblies
-platanus_allee consensus -c assembly.fasta -IP1 BW_R1_paired.fastq BW_R2_paired.fastq -t 46 -o BW_platanus
+platanus_allee consensus -c EC_assembly.fasta -IP1 BW_R1_paired.fastq BW_R2_paired.fastq -t 46 -o BW_platanus
 arcs-make arcs draft=BW_platanus_consensusScaffold_single reads=barcoded
 LINKS -f BW_arcs_scaffolded.fasta -s empty.fof -b BW_scaffolding_ont -l 5 -t 2 -a 0.3
 

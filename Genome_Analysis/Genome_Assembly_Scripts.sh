2c16deddaa60db8397ae65b2bb4d85cd7bb4a47b
Abhisek Chakraborty <chakrabortyabhi2013@gmail.com>
2025-10-11 13:32:44 +0200

    Update Genome_Assembly_Scripts.sh

--- Genome_Analysis/Genome_Assembly_Scripts.sh
+++ Genome_Analysis/Genome_Assembly_Scripts.sh
@@ -2,13 +2,13 @@
 supernova run --id=EC_assembly_output --sample=EC1,ec2,Ec3 --fastqs=EC_input1,EC_input2,EC_input3 --maxreads=all --localcores=40 --localmem=980 --nodebugmem --accept-extreme-coverage
 supernova mkoutput --style=pseudohap2 --asmdir=EC_assembly_output/outs/assembly --outprefix=pseudo2_output_EC --minsize=0 --index
 #Processing of the barcoded reads for downstream usage
-longranger basic --id=longranger_out_EC --fastqs=/DATA/EC/EC_input1,/DATA/EC/EC_input2,/DATA/EC/EC_input3 --sample=EC1,ec2,Ec3 --localcores=36 --localmem=500
+longranger basic --id=longranger_out_EC --fastqs=EC_input1,EC_input2,EC_input3 --sample=EC1,ec2,Ec3 --localcores=36 --localmem=500
 #Correction of the mis-assembly regions using Tigmint
 samtools faidx pseudo2_output_EC.fasta
 bwa index pseudo2_output_EC.fasta
-bwa mem -t46 -p -C pseudo2_output_EC.fasta /DATA/EC/longranger_out_EC/outs/barcoded.fastq.gz | samtools sort -@8 -tBX -o draft.reads.sortbx.bam
+bwa mem -t40 -p -C pseudo2_output_EC.fasta longranger_out_EC/outs/barcoded.fastq.gz | samtools sort -@8 -tBX -o draft.reads.sortbx.bam
 tigmint-molecule draft.reads.sortbx.bam | sort -k1,1 -k2,2n -k3,3n > draft.reads.molecule.bed
-tigmint-cut -p46 -o draft.tigmint.fa pseudo2_output_EC.fasta draft.reads.molecule.bed
+tigmint-cut -p40 -o EC_assembly.fasta pseudo2_output_EC.fasta draft.reads.molecule.bed
 
 ###################Illumina assembly
 #Data-filtration
@@ -18,22 +18,22 @@ spades.py --only-assembler -1 EC_illumina_R1.fastq -2 EC_illumina_R2.fastq -s EC
 
 ###################Scaffolding both 10X Genomics-based and Illumina-based assemblies (Here, the commands are given for only Illumina-based assembly)
 #Using Illumina data
-platanus_allee consensus -c EC_assembly.fasta -IP1 EC_illumina_R1.fastq EC_illumina_R2.fastq -t 46 -o EC_platanus 
+platanus_allee consensus -c EC_assembly.fasta -IP1 EC_illumina_R1.fastq EC_illumina_R2.fastq -t 40 -o EC_platanus 
 #Using 10X Genomics data
 arcs-make arcs draft=EC_platanus_consensusScaffold reads=barcoded_longranger 
-LINKS -f EC_arcs_scaffolded.fasta -s empty.fof -b EC_scaffolding_ont -l 5 -t 2 -a 0.3 
 #Using RNA-Seq data
-/home/Softwares/augustus/bin/augustus --species=fly --gff3=on EC_scaffolding_ont.scaffolds.fa --outfile=augustus_EC_genome
-hisat2-build EC_scaffolding_ont.scaffolds.fa Genome_Index
-hisat2 -x Genome_Index -1 wbT_R1_paired.fastq -2 wbT_R2_paired.fastq -p 40 -S Illumina_Genome.sam
+/home/Softwares/augustus/bin/augustus --species=fly --gff3=on EC_scaffolded_10x.fa --outfile=augustus_EC_genome
+hisat2-build EC_scaffolded_10x.fa Genome_Index
+hisat2 -x Genome_Index -1 EC_RNAseq_R1_paired.fastq -2 EC_RNAseq_R2_paired.fastq -p 40 -S Illumina_Genome.sam
 samtools view -S Illumina_Genome.sam -b -o Illumina_Genome.bam -@ 40
-python2.7 /home/Softwares/AGOUTI/agouti.py scaffold -assembly EC_scaffolding_ont.scaffolds.fa -bam Illumina_Genome.bam -gff augustus_EC_genome.gff3 -outdir ./agouti_output
+python2.7 /home/Softwares/AGOUTI/agouti.py scaffold -assembly EC_scaffolded_10x.fa -bam Illumina_Genome.bam -gff augustus_EC_genome.gff3 -outdir ./agouti_output
+#The scaffolding steps were similarly performed for the Illumina data-based assembly
 
 ###################Merging of the assemblies
-/home/Softwares/quickmerge-0.3-pl526he1b5a44_0/bin/merge_wrapper.py 10X_Genomics_scaffolded.fasta Illumina_scaffolded.fasta -l 38968 -ml 5000
+/home/Softwares/quickmerge-0.3-pl526he1b5a44_0/bin/merge_wrapper.py 10X_Genomics_assembly_AGOUTI_scaffolded.fasta Illumina_assembly_AGOUTI_scaffolded.fasta -l 38968 -ml 5000
 
 ###################Gap-closing
-GapCloser -b config.txt -a Quickmerge_merged.fasta -l 155 -t 46 -o EC_assembly_gapclosed.fasta
+GapCloser -b config.txt -a Quickmerge_merged.fasta -l 155 -t 40 -o EC_assembly_gapclosed.fasta
 config.txt file format:
 #maximal read length
 max_rd_len=155
@@ -52,11 +52,11 @@ q=EC_illumina_unpaired.fastq
 
 ###################Polishing 
 bwa index EC_assembly_gapclosed.fasta
-bwa mem -t 70 -M EC_assembly_gapclosed.fasta EC_illumina_R1.fastq EC_illumina_R2.fastq -o Pilon_EC.sam
-samtools view -S Pilon_EC.sam -b -o Pilon_EC.bam -@ 70
-samtools sort Pilon_EC.bam -o Pilon_EC_sorted.bam -@ 70 -m 10000000000
+bwa mem -t 40 EC_assembly_gapclosed.fasta EC_illumina_R1.fastq EC_illumina_R2.fastq -o Pilon_EC.sam
+samtools view -S Pilon_EC.sam -b -o Pilon_EC.bam -@ 40
+samtools sort Pilon_EC.bam -o Pilon_EC_sorted.bam -@ 40 -m 10000000000
 samtools index Pilon_EC_sorted.bam
-pilon -Xmx950G --genome EC_assembly_gapclosed.fasta --bam Pilon_EC_sorted.bam --output Pilon_output_EC --outdir Pilon_outdir --threads 70 --changes
+pilon -Xmx600G --genome EC_assembly_gapclosed.fasta --bam Pilon_EC_sorted.bam --output Pilon_output_EC --outdir Pilon_outdir --threads 40 --changes
 
 ###################Filtering to keep the contigs >=5 Kb
 perl calc_length_separate.pl Pilon_output_EC.fasta 4999
@@ -64,14 +64,14 @@ mv Pilon_output_EC.fasta.filtered Final_EC_assembly.fasta
 
 ###################Genome assembly quality evaluation
 #Running QUAST
-python /home/Softwares/quast-5.2.0/quast.py --min-contig 0 Final_EC_assembly.fasta -t 60
+python /home/Softwares/quast-5.2.0/quast.py --min-contig 0 Final_EC_assembly.fasta -t 50
 #Running BUSCO
 /home/Softwares/busco-5.4.4/bin/busco --config config.ini -m genome -i Final_EC_assembly.fasta --augustus --augustus_species fly -o EC_BUSCO_output -l Insecta_odb10
 
 ###################Contamination removal from the assembly
 #Mapping the reads onto the genome
 bwa index Final_EC_assembly.fasta
-bwa mem -M Final_EC_assembly.fasta EC_illumina_R1.fastq EC_illumina_R2.fastq -o mapping_output.sam -t 40 
+bwa mem Final_EC_assembly.fasta EC_illumina_R1.fastq EC_illumina_R2.fastq -o mapping_output.sam -t 40 
 /home/anaconda3/bin/samtools view -S mapping_output.sam -b -o mapping_output.bam -@ 40 
 /home/anaconda3/bin/samtools sort mapping_output.bam -o mapping_output_sorted.bam -@ 40 -m 9000000000 
 /home/anaconda3/bin/samtools index mapping_output_sorted.bam
